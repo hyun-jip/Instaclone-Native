ee2b80174c26bb830bd43b54f9bbd482d78de2a9
hyun-jip <frank910@naver.com>
2021-08-04 23:04:57 +0900

    21-08-04

--- apollo.js
+++ apollo.js
@@ -1,9 +1,45 @@
-import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  makeVar,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { offsetLimitPagination } from "@apollo/client/utilities";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const isLoggedInVar = makeVar(false);
+export const tokenVar = makeVar("");
+
+const TOKEN = "token";
+
+export const logUserIn = async (token) => {
+  await AsyncStorage.setItem(TOKEN, token);
+  isLoggedInVar(true);
+  tokenVar(token);
+};
+
+export const logUserOut = async () => {
+  await AsyncStorage.removeItem(TOKEN);
+  isLoggedInVar(false);
+  tokenVar(null);
+};
+
+const httpLink = createHttpLink({
+  uri: "https://wet-shrimp-90.loca.lt/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      token: tokenVar(),
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: "https://hungry-eel-96.loca.lt/graphql",
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 export default client;
--- screens/Feed.js
+++ screens/Feed.js
@@ -1,10 +1,57 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { FlatList, Text, TouchableOpacity, View } from "react-native";
+import { logUserOut } from "../apollo";
+import Photo from "../components/Photo";
+import ScreenLayout from "../components/ScreenLayout";
+import { COMMENT_FRAGMENT, PHOTO_FRAGMENT } from "../fragments";
+
+const FEED_QUERY = gql`
+  query seeFeed {
+    seeFeed {
+      ...PhotoFragment
+      user {
+        username
+        avatar
+      }
+      caption
+      comments {
+        ...CommentFragment
+      }
+      createdAt
+      isMine
+    }
+  }
+  ${PHOTO_FRAGMENT}
+  ${COMMENT_FRAGMENT}
+`;
 
 export default function Feed() {
+  const { data, loading, refetch } = useQuery(FEED_QUERY);
+  const renderPhoto = ({ item: photo }) => {
+    return <Photo {...photo} />;
+  };
+  const refresh = async () => {
+    setRefreshing(true);
+    await refetch();
+    setRefreshing(false);
+  };
+  const [refreshing, setRefreshing] = useState(false);
   return (
-    <View>
-      <Text>Hello</Text>
-    </View>
+    <ScreenLayout loading={loading}>
+      <FlatList
+        refreshing={refreshing}
+        onRefresh={refresh}
+        style={{ width: "100%" }}
+        showsVerticalScrollIndicator={false}
+        data={data?.seeFeed}
+        keyExtractor={(photo) => "" + photo.id}
+        renderItem={renderPhoto}
+      />
+      <TouchableOpacity onPress={() => logUserOut()}>
+        <Text style={{ color: "white" }}>logout</Text>
+      </TouchableOpacity>
+    </ScreenLayout>
   );
 }
